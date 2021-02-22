4807911c7496d3642054062872df90d0a58aaf35
XINLI <lx199317@foxmail.com>
2021-02-22 15:23:38 +0800

    comments

--- AlgorithmPractice/AlgorithmPractice/search.cpp
+++ AlgorithmPractice/AlgorithmPractice/search.cpp
@@ -3,7 +3,14 @@
 using namespace std;
 
 #pragma region sequencesearch
-
+/// <summary>
+/// 顺序查找其实就是遍历
+/// 复杂度O(n)
+/// </summary>
+/// <param name="arr"></param>
+/// <param name="len"></param>
+/// <param name="target"></param>
+/// <returns></returns>
 int sequence_search(int arr[], int len, int target) 
 {
 	for(int i=0; i< len; i++)
@@ -23,6 +30,7 @@ int sequence_search(int arr[], int len, int target)
 /// 二分查找只能用于有序数组
 /// 如果是无序数组需要经历排序
 /// 适合不频繁插入删除的静态查找表
+/// 复杂度O(logn) 最坏情况O(log(n+1))
 /// </summary>
 /// <param name="arr"></param>
 /// <param name="len"></param>
@@ -50,13 +58,43 @@ int binary_search(int arr[], int len, int target)
 
 #pragma endregion
 
+#pragma region insertionsearch
+/// <summary>
+/// 插值查找类似于优化后的二分查找
+/// 二分查找每次都是折一半, 插值查找会根据目标值与当前查找部分的关系计算自适应折点
+/// 插值查找同样只适用于有序数组,适合分布均匀的顺序数组
+/// 复杂度O(log(logn)) 
+/// </summary>
+/// <param name="arr"></param>
+/// <param name="left"></param>
+/// <param name="right"></param>
+/// <param name="target"></param>
+/// <returns></returns>
+int insertion_search(int arr[], int left, int right, int target)
+{
+	int mid = left + (target - arr[left]) * (right - left) / (arr[right] - arr[left]) ;
+	if (arr[mid] == target) {
+		return mid;
+	}
+	else if(arr[mid] > target)
+	{
+		insertion_search(arr, left, mid - 1, target);
+	}
+	else
+	{
+		insertion_search(arr, mid + 1, right, target);
+	}
+}
+
+#pragma endregion
+
 int main()
 {
 	//int arr[] = { 1,1,13,11,6,7,3,34,23,12,5,23,42,63,12,7,26,9,15 };
 	int arr[] = { 1,1,2,4,4,5,6,7,11,13 };
 	int len = (int)sizeof(arr) / (sizeof(*arr));
 	int target = 5;
-	int targetIdx = binary_search(arr, len, target);
+	int targetIdx = insertion_search(arr, 0,len-1, target);
 	cout << targetIdx << endl;
 	return 0;
 }
--- AlgorithmPractice/AlgorithmPractice/sort.cpp
+++ AlgorithmPractice/AlgorithmPractice/sort.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
+//所有排序解释默认为升序排序
 
 #pragma region heapsort
-
+/// <summary>
+/// 堆排序分为大顶堆和小顶堆,对应升序和降序排列
+/// 大顶堆中每个节点的值都大于或等于子节点的值,小顶堆每个节点的值小于等于子节点的值
+/// 排序过程为构建堆,把堆首堆尾元素互换,堆尺寸-1,重新构建堆,并重复之前过程知道堆尺寸=1
+/// 平均复杂度O(n)
+/// </summary>
+/// <param name="arr"></param>
+/// <param name="start"></param>
+/// <param name="end"></param>
 void max_heapify(int arr[], int start, int end) 
 {
 	int dad = start;
@@ -68,7 +77,16 @@ void heap_sort(int arr[], int len)
 #pragma endregion
 
 #pragma region quicksort
-
+/// <summary>
+/// 快速排序使用了分治法来进行排序
+/// 算法过程为选取数组中一个数作为基准数,进行排序,所有比基准数小的都放在它左边,大的都放在右边,称为分区操作
+/// 之后再之前的两区中重新选取基准数并进行刚才的分区操作直到最后所有区内只有一个数,排序完毕
+/// 平均复杂度O(nlogn) 最坏情况O(n平方)
+/// </summary>
+/// <param name="arr"></param>
+/// <param name="left"></param>
+/// <param name="right"></param>
+/// <returns></returns>
 int partition(int arr[], int left, int right) 
 {
 	int pivot = arr[left];
@@ -96,16 +114,22 @@ void quick_sort(int arr[], int left, int right)
 #pragma endregion
 
 #pragma region bubblesort
-
+/// <summary>
+/// 最基础的排序,算法步骤为遍历整个数组,比较相邻两个元素的大小,大的放右边,遍历完成后最大的数在最右边
+/// 接下来重复之前的操作,但是遍历深度-1,即不取数组最后的数,直到最后所有数都经历过了比较
+/// 时间复杂度O(n平方)
+/// </summary>
+/// <param name="arr"></param>
+/// <param name="len"></param>
 void bubble_sort(int arr[], int len) 
 {
 	for (int i =0; i <= len; i++)
 	{
-		for (int j = i ; j <= len; j++) 
+		for (int j = 0 ; j <= len-1-i; j++) 
 		{
-			if (arr[i] > arr[j])
+			if (arr[j] > arr[j+1])
 			{
-				swap(arr[i], arr[j]);
+				swap(arr[j], arr[j+1]);
 			}
 		}
 	}
@@ -114,7 +138,12 @@ void bubble_sort(int arr[], int len)
 #pragma endregion
 
 #pragma region selectsort
-
+/// <summary>
+/// 选择排序算法步骤为遍历数组,选定每次遍历目标数后,遍历数组剩下的数,找到最小的数与遍历目标交换直到数组有序
+/// 时间复杂度O(n平方)
+/// </summary>
+/// <param name="arr"></param>
+/// <param name="len"></param>
 void select_sort(int arr[], int len)
 {
 	for (int i =0; i<= len; i++)
@@ -134,7 +163,12 @@ void select_sort(int arr[], int len)
 #pragma endregion
 
 #pragma region insertsort
-
+/// <summary>
+/// 插入排序的思路为构造一个有序数组,然后从原始数组中取数遍历有序数组,插入对应位置
+/// 复杂度为O(n平方)
+/// </summary>
+/// <param name="arr"></param>
+/// <param name="len"></param>
 void insert_sort(int arr[], int len)
 {
 	for (int i = 1; i <= len; i++)
@@ -157,13 +191,13 @@ void insert_sort(int arr[], int len)
 
 #pragma endregion
 
-int main()
-{
-	int arr[] = { 1,1,13,11,6,7,3,34,23,12,5,23,42,63,12,7,26,9,15 };
-	int len = (int)sizeof(arr) / (sizeof(*arr));
-	insert_sort(arr,len-1);
-	for (int i = 0; i < len; i++) {
-		cout << arr[i] << endl;
-	}
-	return 0;
-}
+//int main()
+//{
+//	int arr[] = { 1,1,13,11,6,7,3,34,23,12,5,23,42,63,12,7,26,9,15 };
+//	int len = (int)sizeof(arr) / (sizeof(*arr));
+//	insert_sort(arr,len-1);
+//	for (int i = 0; i < len; i++) {
+//		cout << arr[i] << endl;
+//	}
+//	return 0;
+//}
